33c1e8671ddd99325fd5eac1f2d76e1bc4a7fd82
Oleksandr Rozumii <alex@rozumiy.name>
2019-04-20 03:52:36 +0300

    Add missing processes autostart on device load

--- lib/magic_home/process_context/process_runner.ex
+++ lib/magic_home/process_context/process_runner.ex
@@ -5,7 +5,18 @@ defmodule MagicHome.ProcessContext.ProcessRunner do
     GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   end
 
-  def init(_params) do
-    {:ok, %{}}
+  def init(_) do
+    GenServer.cast(self(), :run_all)
+
+    {:ok, nil}
   end
+
+  @doc false
+  def handle_cast(:run_all, state) do
+    MagicHome.ProcessContext.list_process_records()
+      |> Enum.each(&(Application.get_env(:magic_home, :bpm_executor).run(&1.id, &1.body)))
+
+    {:noreply, state}
+  end
+
 end
